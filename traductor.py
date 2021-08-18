c7e47663df7d08a138e01dd4e6de570d06be2167
jaime-max <74038168+jaime-max@users.noreply.github.com>
2021-08-17 20:59:28 -0500

    Add files via upload

--- traductor.py
+++ traductor.py
@@ -1,35 +1,68 @@
-#importamos las librerias
 import tkinter as tk
-from  tkinter import ttk
-from googletrans import Translator
+from tkinter import scrolledtext as st
+import googletrans
 
-translator = Translator()
 ventana = tk.Tk()
-ventana.configure(bg="blue")
-ventana.geometry("700x200")
-#funcion para realizar la traduccion
+ventana.title("Traductor de Idiomas")
+ventana.geometry("800x500")
+ventana.resizable(width=False, height=False)
+
+fondo= tk.PhotoImage(file="fondo.png")
+fondo1= tk.Label(ventana, image=fondo).place(x=0, y=0, relwidth=1, relheight=1)
+
+traductor = googletrans.Translator()
+
+#fondo
+boton_fondo= "#004AAD"
+lista_fondo= "#e2e2e2"
+
+caja = st.ScrolledText(ventana, width=37, height=7, relief="flat", font=("Califri Light", 12))
+caja.place(x=20, y=170)
+
+caja2 = tk.Text(ventana, width=37, height=7, relief="flat", font=("Califri Light", 12))
+caja2.place(x=410, y=170)
+
 def traducir():
-    datos= entrada.get()
-    if datos!="":
-    	#idioma de entrada
-        variable_src = combo.get()
-        #idioma de salida
-        variable_dest= combo2.get()
-        #traducimos los datos que ingresa el usuario
-        resultado = translator.translate(datos, src=variable_src, dest=variable_dest)
-        #en una ventana vemos el resultado
-        resultado=tk.Label(ventana,text=resultado.text)
-        respuesta.grid(row=5,column=1)
-Etiqueta=tk.Label(ventana, text="Traductor Conect")
-Etiqueta.grid(row=2,column=1)
-entrada=tk.Entry(ventana)
-entrada.grid(row=3,column=1)
-#seleccionamos el origen del idioma en el cual estamos escribiendo
-combo=ttk.Combobox(ventana,values=['en','es','ru','fr','ca'])
-combo.current(0)
-combo.grid(row=1,column=3)
-#seleccionamos el idioma en el cual queremos que nos tradusca
-combo2=ttk.Combobox(ventana,values=['en','es','ru','fr','ca'])
-combo2.current(1)
-combo2.grid(row=1,column=6)
+    datos = caja.get("1.0", tk.END)
+    traduccion = traductor.translate(datos, dest=idioma2.get())
+    caja2.insert(tk.END, traduccion.text)
+def borrar():
+    caja2.delete("1.0", tk.END)
+
+idioma = tk.StringVar(ventana)
+idioma2 = tk.StringVar(ventana)
+list1 = googletrans.LANGUAGES
+list2 = list1.values()
+list3 = list(list2)
+idiomas= []
+for i in list3:
+    idiomas.append(i)
+
+#droplist = tk.OptionMenu(ventana, idioma, *idiomas)
+#idioma.set("Seleccione el idioma a traducir")
+#droplist.config(width=30, cursor="hand2", relief="flat", bg=lista_fondo, font=("Arial", 12, "bold"))
+#droplist.place(x=74, y=110)
+
+droplist2= tk.OptionMenu(ventana, idioma2, *idiomas)
+idioma2.set("Seleccione el idioma a traducir")
+droplist2.config(width=30, cursor="hand2", relief="flat", bg=lista_fondo, font=("Arial", 12, "bold"))
+droplist2.place(x=425, y=110)
+
+#botones
+boton2 = tk.Button(ventana, text="Traducir", command=traducir, relief="flat", cursor="hand2", bg=boton_fondo,
+height=2, width=14, font=("Arial", 12, "bold"))
+boton2.place(x=99, y=343)
+
+boton3 = tk.Button(ventana, text="Borrar", command=borrar, relief="flat", cursor="hand2", bg=boton_fondo,
+height=2, width=14, font=("Arial", 12, "bold"))
+boton3.place(x=582, y=344)
+
+
+ventana.mainloop()
+
+
+
+
+
+
 
